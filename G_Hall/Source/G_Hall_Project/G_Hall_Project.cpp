37e6871b1c317deab3d5a4d0114fbb5065f1ccdd
mobiusteapot <1947071385@hcs-students.net>
2018-07-19 17:32:28 -0500

    Revert "Brush_conversion"
    This reverts commit bf3e98914600c7670b10e2313a64e02356936730.

--- G_Hall/Source/G_Hall_Project/G_Hall_Project.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "G_Hall_Project.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, G_Hall_Project, "G_Hall_Project" );
