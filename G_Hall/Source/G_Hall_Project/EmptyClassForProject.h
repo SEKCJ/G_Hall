bf3e98914600c7670b10e2313a64e02356936730
mobiusteapot <1947071385@hcs-students.net>
2018-07-19 00:15:09 -0500

    Brush_conversion

---
+++ G_Hall/Source/G_Hall_Project/EmptyClassForProject.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "VRCharacter.h"
+#include "GripMotionControllerComponent.h"
+#include "VRBPDatatypes.h"
+#include "VRGripInterface.h"
+#include "VRSimpleCharacter.h"
+#include "VRPlayerController.h"	
+#include "VRExpansionFunctionLibrary.h"
+#include "GrippableBoxComponent.h"
+#include "VRStereoWidgetComponent.h"
+#include "VRCharacterMovementComponent.h"
+#include "Misc/VRLogComponent.h"
+#include "VRSimpleCharacter.h"
+#include "VRAIController.h"
+#include "VRPathFollowingComponent.h"
+#include "VRRootComponent.h"
+
+/**
+ * 
+ */
+class G_HALL_PROJECT_API EmptyClassForProject
+{
+public:
+	EmptyClassForProject();
+	~EmptyClassForProject();
+
+};
---
+++ G_Hall/Source/G_Hall_Project/G_Hall_Project.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "G_Hall_Project.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, G_Hall_Project, "G_Hall_Project" );
